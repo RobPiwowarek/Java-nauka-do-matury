d9bfb92c7b4167fec68f96406d4e6af3c34d3620
Robert Piwowarek <robpiwowarek@gmail.com>
2015-02-28 15:19:11 +0100

    Update
    Euclidean algorithm for calculating the GCD of two numbers.

---
+++ Euklides.java
@@ -0,0 +1,41 @@
+package naukadomatury;
+
+import java.util.Random;
+
+public class Euklides {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random rand = new Random();
+
+		System.out.println("Najwiekszy wspolny dzielnik to: "
+				+ odejmowanie(rand.nextInt(1000), rand.nextInt(1000)));
+		System.out.println("Najwiekszy wspolny dzielnik to: "
+				+ modulo(rand.nextInt(1000), rand.nextInt(1000)));
+
+	}
+
+	static int odejmowanie(int a, int b) {
+		System.out.println("Liczby: " + a + " i " + b);
+
+		while (a != b && a > 0 && b > 0) {
+			if (a > b)
+				a = a - b;
+			else
+				b = b - a;
+		}
+		return a;
+	}
+
+	static int modulo(int a, int b) {
+		System.out.println("Liczby: " + a + " i " + b);
+
+		while (b != 0) {
+			int c = a % b;
+			a = b;
+			b = c;
+		}
+		return a;
+	}
+
+}
